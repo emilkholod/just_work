b1cdacc21f92676d601f87c50c4945ec901ec061
emilkholod <emilkholod@itlibertas.com>
2023-11-30 21:23:23 +0500

    [IMP] Add API tests

--- mysite/mysite/video_library/tests.py
+++ mysite/mysite/video_library/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase  # noqa: F401
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+client = APIClient()
+
 
 # Create your tests here.
+class TestAPI(TestCase):
+    fixtures = ['initial_data.yaml']
+
+    def test_page_list(self):
+        response = client.get('/api/pages/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()['results']), 3)
+
+    def test_page_instance(self):
+        response = client.get('/api/pages/2/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()['content']), 2)
+
+    def test_content_counter(self):
+        client.get('/api/pages/2/')
+        response = client.get('/api/pages/2/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        contents = response.json()['content']
+        self.assertEqual(contents[0]['counter'], 2)
+        self.assertEqual(contents[1]['counter'], 2)
