b084d0fd2c215e68d6ea349ea83acce5b516fbae
emilkholod <emilkholod@itlibertas.com>
2023-12-01 12:42:25 +0500

    [IMP] Add Page model to admin

--- mysite/mysite/blog/admin.py
+++ mysite/mysite/blog/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class VideoAdminInline(admin.StackedInline):
+    model = models.Video
+    extra = 1
+
+
+class AudioAdminInline(admin.StackedInline):
+    model = models.Audio
+    extra = 1
+
+
+class TextAdminInline(admin.StackedInline):
+    model = models.Text
+    extra = 1
+
+
+@admin.register(models.Page)
+class PageAdmin(admin.ModelAdmin):
+    inlines = (VideoAdminInline, AudioAdminInline, TextAdminInline)
+    search_fields = ['title__istartswith']
