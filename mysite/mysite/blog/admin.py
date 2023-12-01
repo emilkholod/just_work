32444f259310b2edd456580801ee7dc7ce6d9c63
emilkholod <emilkholod@itlibertas.com>
2023-12-01 16:20:53 +0500

    [IMP] Add content models to Django Admin

--- mysite/mysite/blog/admin.py
+++ mysite/mysite/blog/admin.py
@@ -18,7 +18,25 @@ class TextAdminInline(admin.StackedInline):
     extra = 1
 
 
+DEFAULT_SEARCH_FIELDS = ['title__istartswith']
+
+
 @admin.register(models.Page)
 class PageAdmin(admin.ModelAdmin):
     inlines = (VideoAdminInline, AudioAdminInline, TextAdminInline)
-    search_fields = ['title__istartswith']
+    search_fields = DEFAULT_SEARCH_FIELDS
+
+
+@admin.register(models.Video)
+class VideoAdmin(admin.ModelAdmin):
+    search_fields = DEFAULT_SEARCH_FIELDS
+
+
+@admin.register(models.Audio)
+class AudioAdmin(admin.ModelAdmin):
+    search_fields = DEFAULT_SEARCH_FIELDS
+
+
+@admin.register(models.Text)
+class TextAdmin(admin.ModelAdmin):
+    search_fields = DEFAULT_SEARCH_FIELDS
